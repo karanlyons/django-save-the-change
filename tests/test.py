507c368014992a2b698fc045074c420018d42d1b
Karan Lyons <karan@karanlyons.com>
2013-09-08 12:13:48 -0700

    Make test.py use 2to3 build under Python 3.
    This is such a hack.

--- tests/test.py
+++ tests/test.py
@@ -2,10 +2,16 @@
 
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+import imp
 import os
 import sys
 
+if sys.version_info >= (3,):
+	save_the_change = imp.load_source('save_the_change', os.path.abspath(os.path.join(__file__, '..', '..', 'build', 'lib', 'save_the_change', '__init__.py')))
+
+
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'testproject.settings'
 
 from django.test.utils import get_runner
