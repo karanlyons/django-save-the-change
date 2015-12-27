cedd578016c556a9fb8237c8a555de58bbbc71e9
karanlyons <karan@karanlyons.com>
2015-12-27 01:54:37 -0800

    Build TYPES sets correctly in both python 2 & 3.

--- save_the_change/mixins.py
+++ save_the_change/mixins.py
@@ -5,7 +5,6 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 from copy import copy, deepcopy
 from datetime import date, time, datetime, timedelta, tzinfo
 from decimal import Decimal
-from types import NoneType
 from uuid import UUID
 
 from django.conf import settings
@@ -19,17 +18,14 @@ __all__ = ('SaveTheChange', 'TrackChanges')
 
 
 #: A :py:class:`set` listing known immutable types.
-IMMUTABLE_TYPES = set(getattr(settings, 'STC_IMMUTABLE_TYPES', {
-	NoneType, bool, int, float, long, complex, Decimal,
-	str, unicode, basestring,
-	tuple, frozenset,
+IMMUTABLE_TYPES = set(getattr(settings, 'STC_IMMUTABLE_TYPES', (
+	type(None), bool, float, complex, Decimal,
+	six.text_type, six.binary_type, tuple, frozenset,
 	date, time, datetime, timedelta, tzinfo,
 	UUID
-}))
+) + six.integer_types + six.string_types))
 
-INFINITELY_ITERABLE_IMMUTABLE_TYPES = set(getattr(settings, 'STC_INFINITELY_ITERABLE_IMMUTABLE_TYPES', {
-	str, unicode, basestring
-}))
+INFINITELY_ITERABLE_IMMUTABLE_TYPES = set(getattr(settings, 'STC_INFINITELY_ITERABLE_IMMUTABLE_TYPES', (six.text_type, six.binary_type) + six.string_types))
 
 
 class DoesNotExist:
